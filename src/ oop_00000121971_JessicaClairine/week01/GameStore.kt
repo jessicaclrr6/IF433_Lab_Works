7a03ed5c7da01ef29a6514944e18d6f0b3d49cc6
jessica <clairinejessica28@gmail.com>
2026-02-04 22:49:43 +0700

    week01: (tugas 2) implement discount logic

--- src/ oop_00000121971_JessicaClairine/week01/GameStore.kt
+++ src/ oop_00000121971_JessicaClairine/week01/GameStore.kt
@@ -3,4 +3,12 @@ package ` oop_00000121971_JessicaClairine`.week01
 fun main() {
     val gameTitle = "Blockblast"
     val price = 750000
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) {
+        price - (price * 20 / 100)
+    } else {
+        price - (price * 10 / 100)
+    }
