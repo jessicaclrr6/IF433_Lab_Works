65b9a6bd559c0d9d37f7b0dde7697dba925cdc76
jessica <clairinejessica28@gmail.com>
2026-02-04 22:12:24 +0700

    week01: add calculateStatus function

--- src/ oop_00000121971_JessicaClairine/week01/GradingSystem.kt
+++ src/ oop_00000121971_JessicaClairine/week01/GradingSystem.kt
@@ -17,5 +17,8 @@ fun main() {
     }
 
     println("Grade kamu: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
 
+// Tulis DI LUAR main()
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
