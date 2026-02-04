0f44fcc60100325920873fd81dcbf6586f3f33c1
jessica <clairinejessica28@gmail.com>
2026-02-04 21:43:39 +0700

    week01: initial setup and procedural code

---
+++ src/ oop_00000121971_JessicaClairine/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package ` oop_00000121971_JessicaClairine`.week01
+
+fun main() {
+    var name: String = "Jhon Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
