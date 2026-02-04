fb23e0991d0c393def25f70f4f7bc34e689e0b82
jessica <clairinejessica28@gmail.com>
2026-02-04 22:14:52 +0700

    week01: implement null safety check

--- src/ oop_00000121971_JessicaClairine/week01/GradingSystem.kt
+++ src/ oop_00000121971_JessicaClairine/week01/GradingSystem.kt
@@ -18,6 +18,9 @@ fun main() {
 
     println("Grade kamu: $grade")
     println("Status: ${calculateStatus(score)}")
+    val studentId: String? = null
+    val idLength = studentId?.length ?: 0
+    println("Panjang ID: $idLength")
 }
 
 // Tulis DI LUAR main()
