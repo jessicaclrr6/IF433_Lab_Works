0c350508efe4f787c2e4b843ffb49f065dfda3a4
jessica <clairinejessica28@gmail.com>
2026-02-11 15:13:33 +0700

    week02: create basic Student class structure

---
+++ src/oop_00000121971_JessicaClairine/week02/M2ClassF_Theory.kt
@@ -0,0 +1,32 @@
+package oop_00000121971_JessicaClairine.week02
+
+class Pemain {
+    var nama:String = "tidak terdaftar";
+    var speed:Int = 9;
+    val shoot:Int = 8;
+
+    init {
+        this.nama = "messi";
+        println("ini adalah consturctor")
+    }
+
+    constructor(namapanjang:String="Budi", kecepatan:Int=2){
+        nama = namapanjang;
+        speed = kecepatan;
+        println("ini constructor kedua")
+    }
+    fun bawa_bola() {
+        println("si $nama sedang bawa bola")
+    }
+}
+
+fun main () {
+    val player = Pemain("yoel", kecepatan=100);
+    player.bawa_bola();
+    println("nama pemain: ${player.nama}");
+    println("skill Kecepatan: " + player.speed);
+
+    val player2 = Pemain(namapanjang = "ronaldo", kecepatan = 90);
+    println("pemain populer: ${player2.nama}");
+    println("kecepatan: ${player2.speed}");
+}
---
+++ src/oop_00000121971_JessicaClairine/week02/student.kt
@@ -0,0 +1,9 @@
+package oop_00000121971_JessicaClairine.week02
+
+class Student (
+    val name: String,
+    val nim: String,
+    var major: String,
+){
+    //Body class kosong dulu
+}
