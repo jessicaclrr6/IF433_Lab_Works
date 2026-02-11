8d84a2648c3812e79e65b6ed05f557dd65934ba0
jessica <clairinejessica28@gmail.com>
2026-02-11 15:35:22 +0700

    week02: implement user input and pre-validation

---
+++ src/oop_00000121971_JessicaClairine/week02/Main.kt
@@ -0,0 +1,32 @@
+package oop_00000121971_JessicaClairine.week02
+
+package oop_00000121971_JessicaClairine.week02
+
+import java.util.Scanner
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- APLIKASI PMB UMN ---")
+
+    print("Masukan Nama: ")
+    val name = scanner.nextLine()
+
+    println("Masukan NIM (Wajib 5 Karakter): ")
+    val nim = scanner.next()
+
+    scanner.nextLine() // bersihkan buffer newline(penyakit klasik scanner!)
+
+    // validasi di sisi pemanggil (main)
+    if (nim.length != 5) {
+        println("ERROR: Pendaftaran dibatalkan. NIM harus 5 karakter!")
+        // program berheti di sini untuk mahasiswa ini, tidak membuat objek
+    } else {
+        print("Masukan Jurusan: ")
+        val major = scanner.nextLine()
+
+        // instansiasi objek karena data sudah aman
+        val s1 = Student(name, nim, major)
+        println("Status: Pendaftaran Selesai.")
+    }
+}
+
--- src/oop_00000121971_JessicaClairine/week02/student.kt
+++ src/oop_00000121971_JessicaClairine/week02/student.kt
@@ -4,7 +4,7 @@ class Student (val name: String, val nim: String, var major: String){
     init{
         // validasi sederhana: cek panjnag nim
         if (nim.length != 5) {
-            println("WARNING : objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println(" : objek tercipta dengan NIM ($nim) yang tidak valid!")
             println("data mahasiswa $name mungkin akan bermasala di sistem.")
         } else {
             println("LOG: objek student $name berhasil dialokasikan di memory.")
