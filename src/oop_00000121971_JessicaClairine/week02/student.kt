79961935037ae472c80d7b2a391ea68e653507cf
jessica <clairinejessica28@gmail.com>
2026-02-11 15:21:35 +0700

    week02: create basic Student class structure

--- src/oop_00000121971_JessicaClairine/week02/student.kt
+++ src/oop_00000121971_JessicaClairine/week02/student.kt
@@ -1,9 +1,13 @@
 package oop_00000121971_JessicaClairine.week02
 
-class Student (
-    val name: String,
-    val nim: String,
-    var major: String,
-){
-    //Body class kosong dulu
+class Student (val name: String, val nim: String, var major: String){
+    init{
+        // validasi sederhana: cek panjnag nim
+        if (nim.length != 5) {
+            println("WARNING : objek tercipta dengan NIM ($nim) yang tidak valid!")
+            println("data mahasiswa $name mungkin akan bermasala di sistem.")
+        } else {
+            println("LOG: objek student $name berhasil dialokasikan di memory.")
+        }
+    }
 }
