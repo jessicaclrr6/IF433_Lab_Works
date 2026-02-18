17bb2689e7c93c76161945bd3da2db53ba7634ac
jessica <clairinejessica28@gmail.com>
2026-02-18 15:38:57 +0700

    week03: (task 1) implement Weapon testing in main

--- src/ oop_00000121971_JessicaClairine/week01/week03/Main.kt
+++ src/ oop_00000121971_JessicaClairine/week01/week03/Main.kt
@@ -7,4 +7,14 @@ fun main() {
     println("Gaji: ${e.salary}")
     e.increasePerformance()
     println("Pajak yang harus dibayar: ${e.tax}")
+
+    println("\n=== TEST WEAPON ===")
+    val sword = Weapon("Excalibur", 300)
+
+    sword.damage = -50
+    sword.damage = 9999
+
+    println("Nama Weapon: ${sword.name}")
+    println("Damage: ${sword.damage}")
+    println("Tier: ${sword.tier}")
 }
