a1e61814498175d09a6047c4e7de675cb417ca5b
jessica <clairinejessica28@gmail.com>
2026-03-04 14:56:55 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/ oop_00000121971_JessicaClairine/week01/week05/Dosen.kt
@@ -0,0 +1,10 @@
+package ` oop_00000121971_JessicaClairine`.week01.week05
+
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
