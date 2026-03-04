60dfc263b6c1d19b8eabb8d2c24ad0084ba6305c
jessica <clairinejessica28@gmail.com>
2026-03-04 15:10:01 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/ oop_00000121971_JessicaClairine/week01/week05/MathHelper.kt
@@ -0,0 +1,13 @@
+package ` oop_00000121971_JessicaClairine`.week01.week05
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
+    fun hitungLuas(jariJari: Double): Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
