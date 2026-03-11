bf89e1c4b89c5f244e0696c1caa8e99dedc0d79e
jessica <clairinejessica28@gmail.com>
2026-03-11 15:29:47 +0700

    week06: implement decoupled processCheckout in main

---
+++ src/ oop_00000121971_JessicaClairine/week01/week06/Main.kt
@@ -0,0 +1,24 @@
+package ` oop_00000121971_JessicaClairine`.week01.week06
+
+fun processCheckout(method: PaymentMethod, amount: Double) {
+    println("-> Memulai checkout...")
+    method.pay(amount)
+}
+
+fun main() {
+
+    val myWatch = Smartwatch()
+    myWatch.showTime()
+
+    val myPhone = Smartphone()
+    myPhone.turnOn()
+
+    val pay1 = Gopay()
+    val pay2 = CreditCard()
+
+    println("\n=== TESTING CHECKOUT ===")
+
+    processCheckout(pay1,  50000.0)
+    processCheckout(pay2, 150000.0)
+
+}
