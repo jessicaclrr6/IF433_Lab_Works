f9b4e2e6e54d0af4189963605577bd3d923ff21a
jessica <clairinejessica28@gmail.com>
2026-03-11 14:59:40 +0700

    week06: create Button class implementing Clickable

---
+++ src/ oop_00000121971_JessicaClairine/week01/week06/Button.kt
@@ -0,0 +1,7 @@
+package ` oop_00000121971_JessicaClairine`.week01.week06
+
+class Button(override val name: String) : Clickable {
+    override fun click() {
+        println("$name clicked")
+    }
+}
