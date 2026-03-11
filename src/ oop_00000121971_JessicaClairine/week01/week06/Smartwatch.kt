88ea839f51a3549d24ab03ec144a0bb907e6fedb
jessica <clairinejessica28@gmail.com>
2026-03-11 15:08:25 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/ oop_00000121971_JessicaClairine/week01/week06/Smartwatch.kt
@@ -0,0 +1,15 @@
+package ` oop_00000121971_JessicaClairine`.week01.week06
+
+class Smartwatch : Watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkatn HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger magnetik 15W.")
+    }
+}
