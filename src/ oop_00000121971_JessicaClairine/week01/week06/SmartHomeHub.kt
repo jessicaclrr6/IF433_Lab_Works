141e3123c50ff2007b42008e6c16a1ced69a7cf5
jessica <clairinejessica28@gmail.com>
2026-03-11 15:39:30 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/ oop_00000121971_JessicaClairine/week01/week06/SmartHomeHub.kt
@@ -0,0 +1,25 @@
+package ` oop_00000121971_JessicaClairine`.week01.week06
+
+class SmartHomeHub {
+    val devices = mutableListOf<SmartDevice>()
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+    }
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+    fun activateSecurityMode() {
+        for (device in devices) {
+            if (device is Recordable) {
+                device.startRecord()
+            }
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
+            }
+        }
+    }
+}
