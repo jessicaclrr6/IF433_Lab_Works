3d7b0f3c479ac911285cfdda29d4c288ca33aad5
jessica <clairinejessica28@gmail.com>
2026-03-25 15:00:35 +0700

    week07: (trial) trigger private constructor error on NetworkClient

--- src/ oop_00000121971_JessicaClairine/week01/week07/NetworkClient.kt
+++ src/ oop_00000121971_JessicaClairine/week01/week07/NetworkClient.kt
@@ -1,7 +1,15 @@
 package ` oop_00000121971_JessicaClairine`.week01.week07
 
 class NetworkClient private constructor(val url: String) {
+    companion object {
+        const val BASE_URL = "https://api.umn.ac.id"
+
+        fun createClient(): NetworkClient {
+            println("Membangun NetworkClienr dengan BASE_URL: $BASE_URL")
+            return NetworkClient(BASE_URL)
+        }
+    }
     fun connect() {
-        println("Connecting to $url")
+        println("Connected to $url..."
     }
 }
