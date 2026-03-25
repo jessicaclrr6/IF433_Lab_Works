724387c4870292b099498506c2051ccd216cffe2
jessica <clairinejessica28@gmail.com>
2026-03-25 15:05:31 +0700

    week07: test Singleton and Companion Object in main

--- src/ oop_00000121971_JessicaClairine/week01/week07/Main.kt
+++ src/ oop_00000121971_JessicaClairine/week01/week07/Main.kt
@@ -1,5 +1,13 @@
 package ` oop_00000121971_JessicaClairine`.week01.week07
 
+import ` oop_00000121971_JessicaClairine`.week01.week07.DatabaseManager.connectionStatus
+
 fun main() {
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION ONJECT ===")
+    val client = NetworkClient.createClient()
+    client.connect()
 }
