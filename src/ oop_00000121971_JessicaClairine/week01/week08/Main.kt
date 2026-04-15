4f173032ebb22b1d2897afb261cd308c22db0e05
jessica <clairinejessica28@gmail.com>
2026-04-15 15:11:38 +0700

    week08: implement requireNotNull as safer alternative

--- src/ oop_00000121971_JessicaClairine/week01/week08/Main.kt
+++ src/ oop_00000121971_JessicaClairine/week01/week08/Main.kt
@@ -1,5 +1,5 @@
 package ` oop_00000121971_JessicaClairine`.week01.week08
-//jessica
+
 fun main() {
     println("=== TEST SAFE CALLS & ELVIS ===")
     val emptyOrder = Order(null, null)
@@ -39,6 +39,14 @@ fun main() {
         val length = toxicData!!.length
     } catch (e: NullPointerException) {
         println("CRASH (NPE)! Jangan gunakan !1 secara sembarangan")
+    }
 
+    val apiResponse: Map<String, String?> = mapOf("status" to "200", "token" to null)
+    try {
+        val token = requireNotNull(apiResponse["token"]) {
+            "CRITICAL EXECPTION: Token otentikasi tidak ditemukan dari server!"
+        }
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
     }
 }
