0eb1b013ba67fb28d962b71f1d110f5794db0abc
jessica <clairinejessica28@gmail.com>
2026-04-15 15:28:33 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000121971_JessicaClairine/week01/week08/LegacyJavaAPI.java
@@ -0,0 +1,7 @@
+package ` oop_00000121971_JessicaClairine`.week01.week08;
+
+public class LegacyJavaAPI {
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
