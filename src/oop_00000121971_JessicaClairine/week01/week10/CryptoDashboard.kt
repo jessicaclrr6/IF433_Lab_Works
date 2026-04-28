785329d8c6ba8327e62cce724b04164170eebee0
jessica <clairinejessica28@gmail.com>
2026-04-28 22:24:37 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000121971_JessicaClairine/week01/week10/CryptoDashboard.kt
+++ src/oop_00000121971_JessicaClairine/week01/week10/CryptoDashboard.kt
@@ -2,6 +2,7 @@ package oop_00000121971_JessicaClairine.week01.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 10.0))
