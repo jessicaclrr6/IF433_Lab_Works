7c53e3ca3f6b0a17899d8d1184aca32274e64b44
jessica <clairinejessica28@gmail.com>
2026-05-06 09:07:01 +0700

    week11: create string extension functions

---
+++ src/oop_00000121971_JessicaClairine/week01/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_00000121971_JessicaClairine.week01.week11
+
+fun String.addHello(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
