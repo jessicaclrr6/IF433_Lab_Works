0428be06fd52ca5328ace692de8cc4accf96f14c
jessica <clairinejessica28@gmail.com>
2026-05-13 15:06:25 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000121971_JessicaClairine/week01/week12/SmartFeeder.kt
+++ src/oop_00000121971_JessicaClairine/week01/week12/SmartFeeder.kt
@@ -5,5 +5,10 @@ fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): I
     if (isJammed) {
         throw DispenserJamException()
     }
-    return availableGram - requestedGram // Placeholder return
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
