457ae50064f94d5055148884d17a7a4fbec3cb3b
jessica <clairinejessica28@gmail.com>
2026-05-13 15:13:51 +0700

    week12: (task) test full robust smart feeder pipeline

--- src/oop_00000121971_JessicaClairine/week01/week12/SmartFeeder.kt
+++ src/oop_00000121971_JessicaClairine/week01/week12/SmartFeeder.kt
@@ -1,5 +1,5 @@
 package oop_00000121971_JessicaClairine.week01.week12
-
+//jessica
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
     if (isJammed) {
