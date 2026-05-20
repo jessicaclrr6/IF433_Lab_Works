76fb4a99409d2033485d7168ae1fa435f2a48f56
jessica <clairinejessica28@gmail.com>
2026-05-20 22:47:08 +0700

    week13: (task) wrap fromCsvTrade in try-catch for NumberFormatException

--- src/oop_00000121971_JessicaClairine/week01/week13/CryptoTradeLogger.kt
+++ src/oop_00000121971_JessicaClairine/week01/week13/CryptoTradeLogger.kt
@@ -24,6 +24,7 @@ fun fromCsvTrade(line: String): TradeRecord? {
             margin = parts[3].toDouble(),
             pnl = parts[4].toDouble()
         )
+        //error handling
     } catch (e: Exception) {
         println("Log Data korup diabaikan: $line")
         null
