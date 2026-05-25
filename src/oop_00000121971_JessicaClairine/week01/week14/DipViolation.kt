596a7f8dff15202bc0c84f2b951110403b3217cf
jessica <clairinejessica28@gmail.com>
2026-05-25 12:59:26 +0700

    week14: inject Database abstraction to fix DIP

--- src/oop_00000121971_JessicaClairine/week01/week14/DipViolation.kt
+++ src/oop_00000121971_JessicaClairine/week01/week14/DipViolation.kt
@@ -1,2 +1,10 @@
 package oop_00000121971_JessicaClairine.week01.week14
 
+class MySQLDatabase {
+    fun query(sql: String): List<String> = listOf("data1", "data2")
+}
+
+class UserService {
+    private val database = MySQLDatabase()
+    fun getUser(id: Int) = database.query("SELECT * FROM users WHERE id = $id")
+}
