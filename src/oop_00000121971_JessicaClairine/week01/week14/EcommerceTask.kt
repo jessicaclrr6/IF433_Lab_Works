b2fcd9943e9b40babcba28484bf797de9ba7eb73
jessica <clairinejessica28@gmail.com>
2026-05-25 13:15:29 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_00000121971_JessicaClairine/week01/week14/EcommerceTask.kt
+++ src/oop_00000121971_JessicaClairine/week01/week14/EcommerceTask.kt
@@ -1,2 +1,30 @@
 package oop_00000121971_JessicaClairine.week01.week14
+import java.io.File
+
+interface OrderRepository {
+    fun saveOrder(data: String)
+}
+
+class CsvOrderRepository : OrderRepository {
+    override fun saveOrder(data: String) {
+        File("orders.csv").bufferedWriter().use {
+            it.write(data)
+            it.newLine()
+        }
+        println("Order saved to CSV")
+    }
+}
+
+interface NotificationService {
+    fun sendNotification(message: String)
+}
+
+class EmailNotifier : NotificationService {
+    override fun sendNotification(message: String) {
+        println("Email sent: $message")
+    }
+}
+
+class SafeOrderProcessor(private val repo: OrderRepository, private val notifier: NotificationService)
+
 
