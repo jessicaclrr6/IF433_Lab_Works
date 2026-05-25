16430e91253b5e57480fa853987785d40228bb5f
jessica <clairinejessica28@gmail.com>
2026-05-25 10:09:48 +0700

    week14: extract EmailService to fix SRP

--- src/oop_00000121971_JessicaClairine/week01/week14/SrpRefactored.kt
+++ src/oop_00000121971_JessicaClairine/week01/week14/SrpRefactored.kt
@@ -1,2 +1,17 @@
 package oop_00000121971_JessicaClairine.week01.week14
 
+class UserValidator {
+    fun validator(user: User): Boolean  = user.email.contains("@") && user.age >== 18
+}
+
+class UserRepository {
+    fun sae(user: User): User {
+        println("Saving user: ${user.name} to Database")
+    }
+}
+
+class EmailService {
+    fun sendWelcome(user: User) {
+        println("Welcome email -> ${user.email}")
+    }
+}
