ee84b29ed4dfcbfb25573b774eec69c2189f22d5
jessica <clairinejessica28@gmail.com>
2026-05-25 10:44:30 +0700

    week14: refactor SimplePrinter to implement only Printable

--- src/oop_00000121971_JessicaClairine/week01/week14/IspViolation.kt
+++ src/oop_00000121971_JessicaClairine/week01/week14/IspViolation.kt
@@ -1,2 +1,13 @@
 package oop_00000121971_JessicaClairine.week01.week14
 
+interface MultifunctionDevice {
+    fun print(doc: String)
+    fun scan(doc: String): String
+    fun fax(doc: String)
+}
+
+class SimplePrinter : MultifunctionDevice {
+    override fun print(doc: String) = println("Printing: $doc")
+    override fun scan(doc: String): String = throw UnsupportedOperationException("No scanner")
+    override fun fax(doc: String) = throw UnsupportedOperationException("No fax")
+}
