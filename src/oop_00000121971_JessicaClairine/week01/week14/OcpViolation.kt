d3d0bc58f0a5c95ff77579cd1f775dc6de04e5be
jessica <clairinejessica28@gmail.com>
2026-05-25 10:28:18 +0700

    week14: refactor DiscountCalculator to use strategy

--- src/oop_00000121971_JessicaClairine/week01/week14/OcpViolation.kt
+++ src/oop_00000121971_JessicaClairine/week01/week14/OcpViolation.kt
@@ -1,2 +1,12 @@
 package oop_00000121971_JessicaClairine.week01.week14
 
+class DiscountCalculator {
+    fun calculator(price:Double, type: String):Double {
+        return when (type) {
+            "student" -> price * 0.80
+            "member" -> price * 0.85
+            "emplyoee" -> price * 0.70
+            else -> price
+        }
+    }
+}
